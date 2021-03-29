370a958cc9d5cc175b318af54981ac49440d5601
GrzeskoByte <77805280+GrzeskoByte@users.noreply.github.com>
2021-03-29 22:31:48 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    keySession: ['twojklucz'],
-    maxAgeSession: 24 * 60 * 60 * 1000,
-    db: 'mongodb+srv://admin:Dbl0rSUMpyTVMBYD@expressproject.klkjp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
-
